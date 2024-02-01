9c6f637c19b7beaf55f24878a2e9cdcdb8f8e96f
Барышев Степан Сергеевич <Baryshev.S.S@corp.kp11.ru>
2024-02-01 14:40:49 +0300

    0.1

---
+++ Assets/CubeTester.cs
@@ -0,0 +1,73 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CubeTester : MonoBehaviour
+{
+    private bool isActive = false;
+
+    private void OnMouseDown()
+    {
+        if (isActive)
+        {
+            SetDefaultForPlanes();
+        }
+        else
+        {
+            SetEmissionForAccessiblePlanes();
+        }
+    }
+
+    private List<Collider> GetAccessiblePlanes()
+    {
+        Collider[] hits = Physics.OverlapBox(transform.position, transform.localScale);
+
+        List<Collider> result = new List<Collider>();
+
+        foreach (Collider hit in hits)
+        {
+            result.Add(hit);
+        }
+
+        return result;
+    }
+
+    private void SetEmissionForAccessiblePlanes()
+    {
+        var planes = GetAccessiblePlanes();
+
+        foreach (var plane in planes)
+        {
+            if(plane.gameObject.TryGetComponent(out PlaneTester planeTester))
+            {
+                planeTester.SetEmission();
+                planeTester.positionPointMarked += MoveToNewPosition;
+            }
+        }
+
+        isActive = true;
+    }
+
+    private void SetDefaultForPlanes()
+    {
+        var planes = GetAccessiblePlanes();
+
+        foreach (var plane in planes)
+        {
+            if (plane.gameObject.TryGetComponent(out PlaneTester planeTester))
+            {
+                planeTester.SetDefault();
+                planeTester.positionPointMarked -= MoveToNewPosition;
+            }
+        }
+
+        isActive= false;
+    }
+
+    private void MoveToNewPosition(Vector3 planePosition)
+    {
+        SetDefaultForPlanes();
+        transform.position = new Vector3(planePosition.x, transform.position.y, planePosition.z);
+        SetEmissionForAccessiblePlanes();
+    }
+}
